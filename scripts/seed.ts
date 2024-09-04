22170faec6412345e6ad64e69d8ba6196235e8d0
VinhNguyen <vinh@cck.co.jp>
2024-09-04 22:42:49 +0700

    [Update] Seed script

---
+++ scripts/seed.ts
@@ -0,0 +1,53 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+
+import * as schema from '../db/schema';
+
+const sql = neon(process.env.DATABASE_URL!);
+
+const db = drizzle(sql, { schema });
+
+const main = async () => {
+  try {
+    console.log('Seeding database');
+
+    await db.delete(schema.courses);
+    await db.delete(schema.userProgress);
+
+    await db.insert(schema.courses).values([
+      {
+        id: 1,
+        title: 'Spanish',
+        imageSrc: '/es.svg',
+      },
+      {
+        id: 2,
+        title: 'French',
+        imageSrc: '/fr.svg',
+      },
+      {
+        id: 3,
+        title: 'Croatian',
+        imageSrc: '/hr.svg',
+      },
+      {
+        id: 4,
+        title: 'Japanese',
+        imageSrc: '/jp.svg',
+      },
+      {
+        id: 5,
+        title: 'Italian',
+        imageSrc: '/it.svg',
+      },
+    ]);
+
+    console.log('Seeding is finished');
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to seed the database');
+  }
+};
+
+main();
