165f162e9d4595258ecb74f67e39026a3f84d51a
VinhNguyen <vinh@cck.co.jp>
2024-09-21 21:53:44 +0700

    [Implement] Exit modal

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { InfinityIcon, X } from "lucide-react";
 
 import { Progress } from "@/components/ui/progress";
+import { useExitModal } from "@/store/use-exit-modal";
 
 type Props = {
   hearts: number;
@@ -9,10 +10,16 @@ type Props = {
   hasActiveSubscription: boolean;
 }
 
-export const Header = ({ hearts, percentage, hasActiveSubscription }: Props) => {
+export const Header = ({
+  hearts,
+  percentage,
+  hasActiveSubscription
+}: Props) => {
+  const { open } = useExitModal()
+
   return (
     <header className="lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full">
-      <X onClick={() => { }} className="text-slate-500 hover:opacity-75 transition cursor-pointer" />
+      <X onClick={open} className="text-slate-500 hover:opacity-75 transition cursor-pointer" />
       <Progress value={percentage} />
       <div className="text-rose-500 flex items-center font-bold">
         <Image src='/heart.svg' height={28} width={28} alt="Heart" className="mr-2" />
